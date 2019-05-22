2940e066b24e8dc21e47dce71971da831bdf5179
Thiago <1.tnasci@gmail.com>
2019-05-21 23:02:41 -0300

    Novo Projeto

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,20 +2,35 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private LocalTime horario;
+	@ManyToOne
 	private Sala sala;
+	@ManyToOne
 	private Filme filme;
-	
+
+	/***
+	 * @Deprecated hibernate only	 
+	 */
+	public Sessao () {
+		
+	}
 	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.horario = horario;
 		this.filme = filme;
 		this.sala = sala;
-		
-		
-		
+
 	}
 
 	public Integer getId() {
@@ -49,7 +64,5 @@ public class Sessao {
 	public void setFilme(Filme filme) {
 		this.filme = filme;
 	}
-	
-	
 
 }
